2e2a674e327bc2fabe93215af4b6bcbd139b999a
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-01 10:06:03 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-dom/client'], // Adicione esta linha
+    },
+  },
   optimizeDeps: {
     include: ['react-slidedown'],
   },
